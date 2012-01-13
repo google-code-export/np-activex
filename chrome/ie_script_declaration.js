ea9e55f5c81662f9575b9e2b8aab2139e7890213
qiuc12 <qiuc12@gmail.com>
2012-01-13 11:05:06 +0800

    Update to 1.3.11

--- chrome/ie_script_declaration.js
+++ chrome/ie_script_declaration.js
@@ -126,6 +126,17 @@ function __declareClsid(proto) {
   })
 }
 
+function __declareIEUserAgent() {
+  delete navigator.appName;
+  navigator.appName='Microsoft Internet Explorer';
+
+  delete navigator.appVersion;
+  navigator.appVersion = '5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)';
+
+  delete navigator.userAgent;
+  navigator.userAgent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)'
+}
+
 function __initIEScription() {
   __declareActiveXObject();
   __declareEventAsIE(window.Node.prototype);
@@ -133,6 +144,7 @@ function __initIEScription() {
   __declareFakePopup(window.__proto__);
   __declareClsid(HTMLObjectElement.prototype);
   __overloadCreateElement(HTMLDocument.prototype);
+  __declareIEUserAgent();
 }
 
 __initIEScription();
