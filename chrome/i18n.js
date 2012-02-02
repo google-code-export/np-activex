ad2b14d0a32d1170f54b3f0b6b8c3f619a5bf66c
qiuc12 <qiuc12@gmail.com>
2012-02-02 23:10:48 +0800

    Update&reorganize the instructions. Improve Options UI.

--- chrome/i18n.js
+++ chrome/i18n.js
@@ -4,12 +4,12 @@
 
 function loadI18n() {
   document.title = chrome.i18n.getMessage("option_title")
-    var spans = document.getElementsByTagName("span");
+    var spans = document.querySelectorAll('[i18n]');
   for (var i = 0; i < spans.length; ++i) {
     var obj = spans[i];
     if (!obj.hasAttribute("i18n"))
       continue;
-    obj.innerText = chrome.i18n.getMessage(obj.getAttribute("i18n"));
+    obj.innerHTML = chrome.i18n.getMessage(obj.getAttribute("i18n"));
   }
 }
 window.addEventListener("load", loadI18n, false);
