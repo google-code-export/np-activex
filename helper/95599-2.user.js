9f7fff2fb49ea50aa3c51c1faabf20c715ea4487
qiuc12 <qiuc12@gmail.com>
2012-02-09 18:19:37 +0800

    Update 95599 user script

--- helper/95599-2.user.js
+++ helper/95599-2.user.js
@@ -1,9 +1,8 @@
 // ==UserScript==
-// @namespace qiuc12-chrome-adapt
+// @namespace http://code.google.com/p/np-activex
 // @name 95599 Agricultural Bank of China Assist
 // @description 95599 Agricultural Bank of China Assist. Make it able to input password on login.
-// @match https://easyabc.95599.cn/commbank/netBank/zh_CN/CommLogin.aspx
-// @match https://easyabc.95599.cn/SlfRegPer/netBank/zh_CN/entrance/logon.aspx
+// @match https://easyabc.95599.cn/*
 // ==/UserScript==
 
 var maps = document.getElementsByTagName("map");
