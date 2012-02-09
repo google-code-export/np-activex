44bfd48786ea120cbbce03ea00fb62096295cb2b
qiuc12 <qiuc12@gmail.com>
2012-02-09 13:28:49 +0800

    Use port to communicate with page. Loggings save on background page.

--- chrome/inject_start.js
+++ chrome/inject_start.js
@@ -6,6 +6,7 @@ var controlLogFName="__npactivex_log";
 var controlLogEvent="__npactivex_log_event__";
 
 var config = null;
+var port = null;
 
 var logs = [];
 function onControlLog(event) {
@@ -15,31 +16,52 @@ function onControlLog(event) {
 
 window.addEventListener(controlLogEvent, onControlLog, false);
 
+function connect() {
+  if (port) {
+    return;
+  }
+  console.log('Connect');
+  port = chrome.extension.connect();
+  for (var i = 0; i < logs.length; ++i) {
+    port.postMessage({command: 'Log', message: logs[i]});
+  }
+  if (port && config) {
+    logs = undefined;
+  }
+}
+
 function log(message) {
-  if (config == null || config.logEnabled) {
-    if (config != null) {
-      console.log(message);
-    }
+  var time = (new Date()).toLocaleTimeString();
+  message = time + ' ' + message;
+  if (config && config.logEnabled) {
+    console.log(message);
+  }
+
+  if (port) {
+    port.postMessage({command: 'Log', message: message});
+  }
+  if (!config || !port) {
     logs.push(message);
   }
 }
 
+log('PageURL: ' + location.href);
+
 var pendingObjects = [];
 function init(response) {
   config = new ActiveXConfig(response);
+
+  if (config.logEnabled) {
+    for (var i = 0; i < logs.length; ++i) {
+      console.log(logs[i]);
+    }
+  }
 
   eval(config.extScript);
   executeScript(config.pageScript);
 
   setUserAgent();
 
-  if (config.logEnabled) {
-    for (var i = 0; i < logs.length; ++i) {
-      console.log(logs[i]);
-    }
-  } else {
-    logs = [];
-  }
   log('Page rule:' + JSON.stringify(config.pageRule));
   for (var i = 0; i < pendingObjects.length; ++i) {
     pendingObjects[i].activex_process = false;
