b8a25817c95a46803c7229cb031af17129d22b2d
qiuc12 <qiuc12@gmail.com>
2012-02-24 13:35:28 +0800

    Popup page which supports share/auto-enable/issue tracking.

---
+++ chrome/popup.js
@@ -0,0 +1,106 @@
+var tabId = parseInt(location.href.replace(/.*tabid=([0-9]*).*/, '$1'));
+if (isNaN(tabId)) {
+  alert('Invalid tab id');
+}
+
+var backgroundPage = chrome.extension.getBackgroundPage();
+
+var tabInfo = backgroundPage.tabStatus[tabId];
+var setting = backgroundPage.setting;
+if (!tabInfo) {
+  alert('Cannot get tab tabInfo');
+}
+
+$(document).ready(function() {
+  $('.status').hide();
+  $('#submitissue').hide();
+  if (tabInfo.count == 0) {
+    // Shouldn't have this popup
+  } else if (tabInfo.error != 0) {
+    $('#status_error').show();
+  } else if (tabInfo.count != tabInfo.actived) {
+    $('#status_disabled').show();
+  } else {
+    $('#status_ok').show();
+    $('#submitissue').show();
+  }
+});
+
+$(document).ready(function() {
+  $('#issue_view').hide();
+  if (tabInfo.error != 0) {
+    var errorid = tabInfo.issueId;
+    var issue = setting.issues[errorid];
+    $('#issue_desp').text(issue.description);
+    var url = issue.url;
+    if (!url) {
+      var issueUrl = "http://code.google.com/p/np-activex/issues/detail?id=";
+      url = issueUrl + issue.issueId;
+    }
+    $('#issue_track').attr('href', url);
+    $('#issue_view').show();
+  }
+});
+
+function refresh() {
+  alert($$('refresh_needed'));
+}
+
+function showEnableBtns() {
+  var list = $('#enable_btns');
+  list.hide();
+  if (tabInfo.count > tabInfo.actived) {
+    list.show();
+    var info = {actived: true};
+    for (var i = 0; i < tabInfo.frames && info.actived; ++i) {
+      for (var j = 0; j < tabInfo.objs[i].length && info.actived; ++j) {
+        info = tabInfo.objs[i][j];
+      }
+    }
+    if (info.actived) {
+      return;
+    }
+    var rule = setting.getFirstMatchedRule(info, setting.defaultRules);
+    if (rule) {
+      var button = $('<button>').addClass('defaultRule');
+      button.text($$('enable_default_rule', rule.title));
+      button.click(function() {
+        setting.activeRule(rule);
+        refresh();
+      });
+      list.append(button);
+    } else {
+      var site = info.href.replace(/[^:]*:\/\/([^\/]*).*/, '$1');
+      var sitepattern = info.href.replace(/([^:]*:\/\/[^\/]*).*/, '$1/*');
+
+      var btn1 = $('<button>').addClass('customRule').
+      text($$('add_rule_site', site)).click(function() {
+        var rule = setting.createRule();
+        rule.type = 'wild';
+        rule.value = sitepattern;
+        setting.addCustomRule(rule);
+        refresh();
+      });
+
+      var clsid = info.clsid;
+      var btn2 = $('<button>').addClass('customRule').
+      text($$('add_rule_clsid')).click(function() {
+        var rule = setting.createRule();
+        rule.type = 'clsid';
+        rule.value = clsid;
+        setting.addCustomRule(rule);
+        refresh();
+      });
+      list.append(btn1).append(btn2);
+    }
+  }
+}
+
+$(document).ready(function() {
+  $('#submitissue').click(function() {
+    tabInfo.tracking = true;
+    alert($$('issue_submitting'));
+  });
+});
+
+$(document).ready(showEnableBtns);
