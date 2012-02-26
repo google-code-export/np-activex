8a93e4d40d447a9f4a8b41fe3981c226a2d6627d
qiuc12 <qiuc12@gmail.com>
2012-02-26 15:18:06 +0800

    Remove all helper-scripts in this repo.

--- helper/BankOfChina/IEProxy.js
+++
@@ -1,8 +0,0 @@
-delete navigator.appName;
-navigator.appName='Microsoft Internet Explorer';
-
-delete navigator.appVersion;
-navigator.appVersion = '5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)';
-
-delete navigator.userAgent;
-navigator.userAgent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)'
