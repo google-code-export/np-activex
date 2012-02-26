ba9699dd2635025e6bfb13aa95cb1f824a0a7925
qiuc12 <qiuc12@gmail.com>
2012-02-26 13:50:09 +0800

    Remove page_load.js

--- chrome/page_load.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2012 eagleonhill(qiuc12@gmail.com). All rights reserved.
-// Use of this source code is governed by a Mozilla-1.1 license that can be
-// found in the LICENSE file.
-
-chrome.extension.sendRequest({command: 'ResetPageIcon'});
