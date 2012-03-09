c9e5ec71da4bdac79d23b94238c83867a6833401
Chuan Qiu <Chuan Qiu@ChuanQiu-PC>
2012-03-09 16:45:11 +0800

    Log error messages.

--- chrome/inject_start.js
+++ chrome/inject_start.js
@@ -109,3 +109,4 @@ chrome.extension.sendRequest(
   {command:"Configuration", href:location.href, top: self == top}, loadConfig);
 
 window.addEventListener("beforeload", onBeforeLoading, true);
+window.addEventListener('error', onError, true);
