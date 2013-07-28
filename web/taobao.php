479edeb1463ecedc60f97c8cec125980d117f503
Chuan Qiu <Chuan Qiu@localhost>
2013-07-28 16:52:40 -0400

    Output user-message for taobao

--- web/taobao.php
+++ web/taobao.php
@@ -1,6 +1,6 @@
 <?php
 function load() {
-  header('Content-Type: text/text; charset=GBK');
+  header('Content-Type: text/text; charset=UTF-8');
   $code = $_GET['code'];
   if ($code == "") {
     header('Location: https://oauth.taobao.com/authorize?response_type=code&redirect_uri=http://eagleonhill.oicp.net/editor/taobao.php&client_id=21134472');
