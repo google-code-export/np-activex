5f186d5f4bc1b22d1f653436c2b81d6629f15a38
Chuan Qiu <Chuan Qiu@localhost>
2013-10-26 23:48:16 -0700

    New welcome page for basic instructions and donation.

--- chrome/donate.js
+++
@@ -1,6 +0,0 @@
-if (chrome.extension.getBackgroundPage().firstRun) {
-  chrome.extension.getBackgroundPage().firstRun = false;
-}
-$(document).ready(function() {
-  $('#share').load('share.html');
-});
